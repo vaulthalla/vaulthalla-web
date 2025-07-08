07178ccc79d6e3b1602cebb65b4ba526a06730f7
Cooper Larson <cooper.larson1@gmail.com>
2025-07-08 15:38:16 -0600

    logo onClick to dashboard

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,19 @@
 import NextImage from 'next/image'
 import VaulthallaLogo from '@/public/vaulthalla-logo.png'
+import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <NextImage
-      src={VaulthallaLogo}
-      alt="Vaulthalla Logo"
-      width={100}
-      height={100}
-      priority
-      className="mx-auto mb-4 h-auto w-auto"
-    />
+    <Link href="/dashboard" className="m-0">
+      <NextImage
+        src={VaulthallaLogo}
+        alt="Vaulthalla Logo"
+        width={100}
+        height={100}
+        priority
+        className="mx-auto mb-4 h-auto w-auto"
+      />
+    </Link>
   )
 }
 
